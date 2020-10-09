d0243e070992887f7ef8de020d86053d4a67fe2e
frabaffo <banfi.francesco@iisgalvanimi.edu.it>
2020-10-09 09:27:37 +0000

    angular

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent,
+   FooComponent
+ ],
+ imports: [
+   BrowserModule, HttpClientModule //Aggiungi il modulo http qui 
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
